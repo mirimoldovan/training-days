cc1e2ba1264477c8ee9e946039441c28c018bb9e
Miriam Moldovan <mirybabutia@yahoo.com>
2021-04-07 13:33:30 +0300

    first commit

---
+++ OneDrive/Desktop/training days/training.js
@@ -0,0 +1,50 @@
+const random = Math.floor(Math.random() * 3);
+ 
+const getRandEvent = () => {
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+ 
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+ 
+  if (event === 'Marathon') {
+   days= 50 ;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+  days = 200;
+  }
+ 
+  return days;
+};
+ 
+// The scope of `name` is too tight 
+const logEvent = (name, event) => {
+     console.log(`${name}'s event is: ${event}`);
+};
+ 
+const logTime = (name, days) => {
+  
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+ 
+const event1 = getRandEvent();
+const days1 = getTrainingDays(event1);
+const name1 = 'Nala';
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+ 
+ 
+logEvent(name1, event1);
+logTime(name1,days1);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+
+logEvent(name2, event2);
+logTime(name2, days2);
